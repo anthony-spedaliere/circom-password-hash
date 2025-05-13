2e319f0b77459ca68f7db25bc75f7c8418d1f7d1
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-12 19:58:09 -0700

    set up basic hardhat project files

---
+++ hardhat-project/hardhat.config.js
@@ -0,0 +1,14 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
